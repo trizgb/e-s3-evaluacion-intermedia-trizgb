8bcb194060a2a65294c5deacc1caee483d5ac29e
trizgb <beatrizgomez92@gmail.com>
2019-01-14 09:40:17 +0100

    Doing CSS, centering image

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Pokemon from './Pokemon';
+// import Pokemon from './Pokemon';
 
 class PokeList extends Component {
 
@@ -9,6 +9,13 @@ class PokeList extends Component {
             <React.Fragment>
                 <img className="pokemonImage" src={item.url} alt={item.name}/>
                 <h2 className="pokemonName">{item.name}</h2>
+                <ul className="typeList">
+                {item.types.map ((type, index) => {
+                    return (
+                        <li  className="type" key={index}>{type}</li>
+                    )
+                })}
+                </ul>
             </React.Fragment>
 
         );
